5aada320682272c0185d67a00b107d78b70c6097
jeremy di dio <jdidio@jeremys-macbook-pro-6.home>
2023-12-10 19:37:09 +0100

    fix downloader

--- src/scripts/data_downloader.py
+++ src/scripts/data_downloader.py
@@ -2,10 +2,11 @@
 Download data from CoinGecko API
 
 Usage: 
-data_downloader.py --data=<path>
+data_downloader.py --data=<path> --batch=<int>
 
 Options:
     --data=<path>   Path to the data folder
+    
 """
 
 
@@ -16,76 +17,93 @@ import time
 import pandas as pd
 from docopt import docopt
 from pathlib import Path
+import datetime
+import numpy as np
 
 
-def main(data_src: Path):
+def main(data_src: Path, batch: int):
     with open((data_src / "id_coins.csv"), newline="") as f:
         reader = csv.reader(f)
         coins_id = list(reader)[0]
 
+    coins_id = coins_id[batch * 100 - 100 : batch * 100]
+
     data = {"vs_currency": "usd", "days": "max"}
-    headers = {"Content-type": "application/json"}
     count = 0
 
-    prices = pd.DataFrame()
-    volumes = pd.DataFrame()
-    market_caps = pd.DataFrame()
+    p = pd.DataFrame()
+    v = pd.DataFrame()
+    m = pd.DataFrame()
 
     for id in tqdm(coins_id):
-        if count == 5:
-            time.sleep(60)
-            count = 0
+        START = datetime.datetime(2020, 1, 2)
+        END = datetime.datetime(2023, 1, 2)
+        current = START
+        count = 0
+        prices = []
+        volumes = []
+        market_caps = []
+        time.sleep(40)
+        while current < END:
+            if count >= 6:
+                time.sleep(40)
+                count = 0
 
-        url = f"https://api.coingecko.com/api/v3/coins/{id}/market_chart"
-        response = requests.get(url, headers=headers, params=data)
-        count += 1
+            _from = current
+            _to = current + datetime.timedelta(days=90)
+            data = {
+                "vs_currency": "usd",
+                "from": _from.timestamp(),
+                "to": _to.timestamp(),
+            }
+            url = f"https://api.coingecko.com/api/v3/coins/{id}/market_chart/range"
+            try:
+                response = requests.get(url, params=data)
+                prices += response.json()["prices"]
+                volumes += response.json()["total_volumes"]
+                market_caps += response.json()["market_caps"]
+                current = _to
+                count += 1
+            except Exception as e:
+                print(e)
+                time.sleep(60)
+                continue
 
-        try:
-            index = [pd.to_datetime(x[0], unit="ms") for x in response.json()["prices"]]
-            prices = pd.concat(
-                [
-                    prices,
-                    pd.DataFrame(
-                        data=[x[1] for x in response.json()["prices"]],
-                        index=index,
-                        columns=[id],
-                    ),
-                ],
-                axis=1,
-            )
-            volumes = pd.concat(
-                [
-                    volumes,
-                    pd.DataFrame(
-                        data=[x[1] for x in response.json()["total_volumes"]],
-                        index=index,
-                        columns=[id],
-                    ),
-                ],
-                axis=1,
-            )
-            market_caps = pd.concat(
-                [
-                    market_caps,
-                    pd.DataFrame(
-                        data=[x[1] for x in response.json()["market_caps"]],
-                        index=index,
-                        columns=[id],
-                    ),
-                ],
-                axis=1,
-            )
+        price_df = pd.DataFrame(
+            prices,
+            columns=["date", id],
+            index=[pd.to_datetime(x[0], unit="ms") for x in prices],
+        ).drop(columns=["date"])
+        volume_df = pd.DataFrame(
+            volumes,
+            columns=["date", id],
+            index=[pd.to_datetime(x[0], unit="ms") for x in volumes],
+        ).drop(columns=["date"])
+        market_cap_df = pd.DataFrame(
+            market_caps,
+            columns=["date", id],
+            index=[pd.to_datetime(x[0], unit="ms") for x in market_caps],
+        ).drop(columns=["date"])
+        p = pd.concat([p, price_df], axis=1)
+        v = pd.concat([v, volume_df], axis=1)
+        m = pd.concat([m, market_cap_df], axis=1)
 
-        except Exception as e:
-            print(e, response.json())
-            break
+    def clean_df(df):
+        df.index = df.index.round("H")
+        df = df.groupby(df.index).mean()
+        return df.replace(0, np.nan)
 
-    prices.to_csv(data_src / "prices.csv")
-    volumes.to_csv(data_src / "volumes.csv")
-    market_caps.to_csv(data_src / "market_caps.csv")
+    p = clean_df(p)
+    v = clean_df(v)
+    m = clean_df(m)
+
+    p.to_csv(f"../data/prices_{batch}.csv")
+    v.to_csv(f"../data/volumes_{batch}.csv")
+    m.to_csv(f"../data/market_caps_{batch}.csv")
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
     data_src = Path(args["--data"])
-    main(data_src)
+    batch = int(args["--batch"])
+    main(data_src, batch)
